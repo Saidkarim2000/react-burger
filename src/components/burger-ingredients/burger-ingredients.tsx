84bb92c7c4080e5ade76515791aa6bb606da17e2
saidkarim2000 <yuldashev754@gmail.com>
2026-08-17 19:46:11 +0500

    Сделана верстка компонента BurgerIngredients

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,4 +1,4 @@
-import { Tab } from '@krgaa/react-developer-burger-ui-components';
+import { Tab, Counter, CurrencyIcon } from '@krgaa/react-developer-burger-ui-components';
 
 import type { TIngredient } from '@utils/types';
 
@@ -12,6 +12,10 @@ export const BurgerIngredients = ({
   ingredients,
 }: TBurgerIngredientsProps): React.JSX.Element => {
   console.log(ingredients);
+
+  const buns = ingredients.filter((ingredient) => ingredient.type === 'bun');
+  const mains = ingredients.filter((ingredient) => ingredient.type === 'main');
+  const sauces = ingredients.filter((ingredient) => ingredient.type === 'sauce');
 
   return (
     <section className={styles.burger_ingredients}>
@@ -46,6 +50,59 @@ export const BurgerIngredients = ({
           </Tab>
         </ul>
       </nav>
+
+      <main className="custom-scroll">
+        <div className={styles.menu_section}>
+          <header className="text text_type_main-medium">Булки</header>
+          <div className={styles.menu_section_block}>
+            {buns.map((bun) => (
+              <li key={bun._id}>
+                <img src={bun.image} alt={bun.name} />
+                <Counter count={1} size="default" extraClass="" />
+                <div className={styles.menu_item_price}>
+                  <h3 className="text text_type_main-default">{bun.price}</h3>
+                  <CurrencyIcon type="primary" />
+                </div>
+                <span>{bun.name}</span>
+              </li>
+            ))}
+          </div>
+        </div>
+
+        <div className={styles.menu_section}>
+          <header className="text text_type_main-medium">Соусы</header>
+          <div className={styles.menu_section_block}>
+            {sauces.map((sauce) => (
+              <li key={sauce._id}>
+                <img src={sauce.image} alt={sauce.name} />
+                <Counter count={1} size="default" extraClass="" />
+                <div className={styles.menu_item_price}>
+                  <h3 className="text text_type_main-default">{sauce.price}</h3>
+                  <CurrencyIcon type="primary" />
+                </div>
+                <span>{sauce.name}</span>
+              </li>
+            ))}
+          </div>
+        </div>
+
+        <div className={styles.menu_section}>
+          <header className="text text_type_main-medium">Начинки</header>
+          <div className={styles.menu_section_block}>
+            {mains.map((main) => (
+              <li key={main._id}>
+                <img src={main.image} alt={main.name} />
+                <Counter count={1} size="default" extraClass="" />
+                <div className={styles.menu_item_price}>
+                  <h3 className="text text_type_main-default">{main.price}</h3>
+                  <CurrencyIcon type="primary" />
+                </div>
+                <span>{main.name}</span>
+              </li>
+            ))}
+          </div>
+        </div>
+      </main>
     </section>
   );
 };
