190775818253092018e3c62bd89ef0872fef9c59
saidkarim2000 <yuldashev754@gmail.com>
2026-09-03 12:11:26 +0500

    Сделан 2 этап: подключение к api

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,48 @@
+import { API } from '@/utils/api/client';
+import { Preloader } from '@krgaa/react-developer-burger-ui-components';
+import { useState, useEffect } from 'react';
+
 import { AppHeader } from '@components/app-header/app-header';
 import { BurgerConstructor } from '@components/burger-constructor/burger-constructor';
 import { BurgerIngredients } from '@components/burger-ingredients/burger-ingredients';
-import { ingredients } from '@utils/ingredients';
+
+import type { TIngredient } from '@/utils/types';
 
 import styles from './app.module.css';
 
+type TIngredientsResponse = {
+  data: TIngredient[];
+};
+
 export const App = (): React.JSX.Element => {
+  const [result, setResult] = useState<TIngredient[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch(API.url, {
+      signal,
+    })
+      .then((resp) => resp.json() as Promise<TIngredientsResponse>)
+      .then((resp) => setResult(resp.data))
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+
+    return (): void => {
+      controller.abort();
+    };
+  }, []);
+
+  if (isLoading) {
+    return <Preloader />;
+  }
+
   return (
     <div className={styles.app}>
       <AppHeader />
@@ -13,8 +50,8 @@ export const App = (): React.JSX.Element => {
         Соберите бургер
       </h1>
       <main className={`${styles.main} pl-5 pr-5`}>
-        <BurgerIngredients ingredients={ingredients} />
-        <BurgerConstructor ingredients={ingredients} />
+        <BurgerIngredients ingredients={result} />
+        <BurgerConstructor ingredients={result} />
       </main>
     </div>
   );
